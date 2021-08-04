2e6791b370795a401c132f3ff54f1d90fb9e9384
nathangobinet <nathan.gobinet@gmail.com>
2021-08-05 00:08:53 +0200

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
+  root: true,
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
   plugins: [
-    '@typescript-eslint',
+      '@typescript-eslint',
+  ],
+  extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
   ],
   rules: {
-  },
+      'lines-between-class-members': 0,
+  }
 };
