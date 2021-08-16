a44f5c20dd8b4383a0c92c241f4793752c944e9d
Nathan Gobinet <nathan.gobinet@thalesgroup.com>
2021-08-16 16:00:53 +0200

    Create a relative mouseEvent in EventManager

--- src/EventManager.ts
+++ src/EventManager.ts
@@ -7,8 +7,22 @@ class EventManager {
   }
 
   private static registerCanvasEvents(canvasElement: HTMLCanvasElement) : void {
-    canvasElement.addEventListener('mousemove', (event : MouseEvent) => shapeManager.updateShape(event));
-    canvasElement.addEventListener('mousedown', (event : MouseEvent) => shapeManager.beginShape(event));
+    canvasElement.addEventListener(
+      'mousemove',
+      (event : MouseEvent) => {
+        const relEvent = this.getRelativeEvent(canvasElement, event);
+        shapeManager.updateShape(relEvent);
+      },
+    );
+
+    canvasElement.addEventListener(
+      'mousedown',
+      (event : MouseEvent) => {
+        const relEvent = this.getRelativeEvent(canvasElement, event);
+        shapeManager.beginShape(relEvent);
+      },
+    );
+
     canvasElement.addEventListener('mouseup', () => shapeManager.endShape());
     canvasElement.addEventListener('mouseleave', () => shapeManager.endShape());
   }
@@ -32,6 +46,14 @@ class EventManager {
         shapeManager.redo();
       }
     });
+  }
+
+  private static getRelativeEvent(canvasEl: HTMLCanvasElement, event: MouseEvent) : MouseEvent {
+    return {
+      clientX: event.clientX - canvasEl.offsetLeft,
+      clientY: event.clientY - canvasEl.offsetTop,
+      ...event,
+    };
   }
 }
 
