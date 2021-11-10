7a22e457ce2e24ddb0a8814abf7991f301793a7d
Nathan Gobinet <nathan.gobinet@gmail.com>
2021-11-09 23:49:23 -0500

    Handle draw permission

---
+++ src/Manager/PermissionManager.ts
@@ -0,0 +1,15 @@
+import drawState from '../DrawState';
+import { PermissionParameter } from '../Permission';
+import ActionType from '../Action/ActionType';
+import IAction from '../Action/IAction';
+import IObserver from '../Observer/IObserver';
+
+class PermissionManager implements IObserver<IAction> {
+  update(elem: IAction): void {
+    if (elem.type !== ActionType.permission) return;
+    const permissionParameter = elem.parameters as PermissionParameter;
+    drawState.drawPermission = permissionParameter.type;
+  }
+}
+
+export default PermissionManager;
